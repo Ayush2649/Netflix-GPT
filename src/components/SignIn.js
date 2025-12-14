bb7bf1f7434df6860d06b43101fb084b750b3d61
Ayush Sahu <ayushsahu1430@gmail.com>
2025-12-14 11:26:37 +0530

    Fixed a small typo

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,4 +1,4 @@
-import React, { use, useState } from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import { useNavigate, useLocation } from "react-router-dom";
 import { validateSignIn } from "../utils/validate";
